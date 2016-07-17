52c759acdaae0ce543451b2b8223b7e5a70b261c
Pedro Morales Rodriguez <Fyx@MacBook-Pro-de-Pedro.local>
2016-07-17 17:47:05 +0200

    Added object with data for parsing

--- jsmn.js
+++ jsmn.js
@@ -1,4 +1,4 @@
-// EXAMPLE
+	// EXAMPLE
 
 // .thisIsAnObject
 // 	thisIsaProperty: "This is a string"
@@ -22,10 +22,10 @@ const fs = require('fs')
 
 function readFile(file) {
 	const readFile = fs.readFileSync(file)
+
 	// Transform bufer to readable format
 	return readFile.toString('utf-8')
 }
-
 
 // Read line by line
 function splitByLines(file) {
@@ -33,28 +33,42 @@ function splitByLines(file) {
 	return file.split(/[\n\r]/g)
 }
 
-
 // Check if something is nested.
 	// Count how many tabs
-function checkLevel(line) {
+function sanitize(line) {
 	const choppedLine = line.split('\t')
 	return {
 		// Indentation level
 		level: choppedLine.length,
 		// Content of line after indentation
-		content: choppedLine.filter(item => item.length > 0).reduce(item => item)
+		content: choppedLine
+			.filter(item => item.length > 0)
+			.reduce(item => item)
 	}
 }
 
 // Parse into JSON
 
-	// Check if tabs are more same or less than previous - If they are more, then get last object as parent.
-// Check type of content
-	// Variable
-	// Array
-	// Object
+function parsedContent(line) {
+	// Get line object 
+	// If content is # set as array, slice the # 
+	switch (line.content[0]) {
+		case '#':
+			line.type = 'array'
+			line.content = line.content.slice(1, -1)
+			break
+	// If content is . set as object, slice the .
+		case '.':
+			line.type = 'object'
+			line.content = line.content.slice(1, -1)
+			break
+	// If content start with letter set as prop, no slice.
+		default:
+			line.type = 'property'		
+	}
 
-
+	return line
+}
 
 // EXECUTION
 
@@ -63,5 +77,5 @@ const lines = splitByLines(readFile('./file.jsmn'))
 
 // For every line create an objext with the level and the content
 for (var i = 0; i < lines.length; i++) {
-	console.log(checkLevel(lines[i]))
+	console.log(parsedContent(sanitize(lines[i])))
 }
